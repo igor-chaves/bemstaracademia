79916b7323f0108edb59fbe77b00bf11122ce735
Igor Chaves Donega <ichaves@live.com>
2025-03-30 17:11:49 +0900

    implements infinite scroll on HealthBenefits component

--- components/ui/HealthBenefits.tsx
+++ components/ui/HealthBenefits.tsx
@@ -1,23 +1,23 @@
 const HealthBenefits = () => {
 	const benefitsData = [
 		{
-			number: "+20%",
+			number: "-20%",
 			benefit: "doenças cardíacas",
-			text: "Estudos indicam que atividades regulares ajudam a prevenir doenças cardíacas.",
+			text: "Segundos estudos, atividades regulares ajudam a prevenir doenças cardíacas.",
 		},
 		{
 			number: "+25%",
 			benefit: "massa muscular",
-			text: "Treinamento de força pode aumentar a massa muscular em até 25% em 12 semanas.",
+			text: "Treinamento de força pode aumentar a massa muscular em 12 semanas.",
 		},
 		{
 			number: "-50%",
-			benefit: "ansiedade e estresse",
+			benefit: "ansiedade/estresse",
 			text: "Exercícios aeróbicos reduzem em 50% os sintomas de ansiedade e estresse.",
 		},
 		{
 			number: "+30%",
-			benefit: "qualidade de sono",
+			benefit: "melhor sono",
 			text: "Exercícios regulares podem melhorar a qualidade do sono em até 30%.",
 		},
 		{
@@ -26,26 +26,34 @@ const HealthBenefits = () => {
 			text: "Atividade física regular pode aumentar a expectativa de vida em até 10 anos.",
 		},
 	]
+
+	const repeatedBenefitsData = [...benefitsData, ...benefitsData]
+
 	return (
 		<>
 			<h3 className="lg:text-7xl md:text-6xl text-4xl text-transparent bg-clip-text bg-gradient-to-b from-orange-200 to-orange-400 font-bold mb-2 pb-4">
-				Benefícios para seu corpo
+				Benefícios para o corpo
 			</h3>
 
-			<div className="flex gap-4 overflow-x-auto">
-				{benefitsData.map(({ number, benefit, text }) => (
-					<div className="md:max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg p-4 min-h-[250px] flex flex-col justify-center mb-8">
-						<div className="mb-4 md:flex md:w-72 w-56">
-							<p className="text-5xl font-semibold text-orange-600 md:mr-2">
-								{number}
-							</p>
-							<p className="text-slate-800 font-bold uppercase flex flex-col justify-end text-medium">
-								{benefit}
-							</p>
+			<div className="relative overflow-x-hidden">
+				<div className="flex gap-4 animate-scroll wrapper">
+					{repeatedBenefitsData.map(({ number, benefit, text }, index) => (
+						<div
+							key={index}
+							className={`md:max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg p-4 min-h-[250px] flex flex-col justify-center mb-8 mx-2 item item${index}`}
+						>
+							<div className="mb-4 md:flex md:w-72 w-56">
+								<p className="text-5xl font-semibold text-orange-600 md:mr-2">
+									{number}
+								</p>
+								<p className="text-slate-800 font-bold uppercase flex flex-col justify-end text-sm">
+									{benefit}
+								</p>
+							</div>
+							<span className="text-sm md:text-base text-gray-600">{text}</span>
 						</div>
-						<span className="text-sm md:text-base text-gray-600">{text}</span>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</>
 	)
