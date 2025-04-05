1eb90d254d0bdaf1db653f14d128d5ee5d254bf0
Igor Chaves Donega <ichaves@live.com>
2025-04-05 12:12:03 +0900

    fix probem with parallax on smarphones

--- components/ui/Parallax.tsx
+++ components/ui/Parallax.tsx
@@ -1,12 +1,14 @@
 const Parallax = () => {
 	return (
-		<div className="w-full h-[400px] flex items-center justify-center relative bg-[url('/parallax.avif')] bg-cover bg-center bg-fixed">
-			<div className="relative bg-white/50 p-5 rounded-lg m-8">
-				<blockquote className="relative max-w-[500px] px-10 py-2 my-1 text-2xl italic font-serif leading-relaxed text-slate-800 flex flex-col justify-center items-center text-center">
+		<div className="w-full h-[400px] flex items-center justify-center bg-[url('/parallax.avif')] bg-cover bg-center md:bg-fixed">
+			<div className="bg-white/50 p-5 rounded-lg m-8">
+				<blockquote className="relative max-w-[500px] px-10 py-2 my-1 text-2xl italic font-serif leading-relaxed text-slate-800 text-center">
 					<span className="absolute text-[80px] text-orange-600 -left-5 -top-11">
 						“
 					</span>
-					<p className="md:text-3xl text-xl">Movimente-se, supere limites e conquiste o seu bem-estar!</p>
+					<p className="md:text-3xl text-xl">
+						Movimente-se, supere limites e conquiste o seu bem-estar!
+					</p>
 				</blockquote>
 			</div>
 		</div>
